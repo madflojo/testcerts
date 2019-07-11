7aaea173f5fe2c4d1483d56a8f2a80a6e7796552
Benjamin Cane <ben@bencane.com>
2019-07-11 12:26:48 -0700

    Cleaner version

--- testcerts.go
+++ testcerts.go
@@ -10,8 +10,8 @@ import (
 	"crypto/x509/pkix"
 	"encoding/pem"
 	"fmt"
-	"github.com/Pallinder/sillyname-go"
 	"math/big"
+	"os"
 	"time"
 )
 
@@ -21,10 +21,53 @@ import (
 //		// do stuff
 //	}
 func GenerateCerts() ([]byte, []byte, error) {
+	// Create certs and return as []byte
+	c, k, err := genCerts()
+	if err != nil {
+		return nil, nil, err
+	}
+	return pem.EncodeToMemory(c), pem.EncodeToMemory(k), nil
+}
+
+// GenerateCertsToFile will create a temporary x509 Certificate and Key files that can be used for testing.
+func GenerateCertsToFile(certFile, keyFile string) error {
+	// Create Certs
+	c, k, err := genCerts()
+	if err != nil {
+		return err
+	}
+
+	// Write to Certificate File
+	cfh, err := os.Create(certFile)
+	if err != nil {
+		return fmt.Errorf("unable to create certificate file - %s", err)
+	}
+	defer cfh.Close()
+	err = pem.Encode(cfh, c)
+	if err != nil {
+		return fmt.Errorf("unable to create certificate file - %s", err)
+	}
+
+	// Write to Key File
+	kfh, err := os.Create(keyFile)
+	if err != nil {
+		return fmt.Errorf("unable to create certificate file - %s", err)
+	}
+	defer kfh.Close()
+	err = pem.Encode(kfh, k)
+	if err != nil {
+		return fmt.Errorf("unable to create certificate file - %s", err)
+	}
+
+	return nil
+}
+
+// genCerts will perform the task of creating a temporary Certificate and Key.
+func genCerts() (*pem.Block, *pem.Block, error) {
 	// Create a Certificate Authority Cert
 	ca := &x509.Certificate{
 		Subject: pkix.Name{
-			Organization: []string{sillyname.GenerateStupidName() + " Inc."},
+			Organization: []string{"Never Use this Certificate in Production Inc."},
 		},
 		SerialNumber:          big.NewInt(42),
 		NotAfter:              time.Now().Add(2 * time.Hour),
@@ -47,8 +90,8 @@ func GenerateCerts() ([]byte, []byte, error) {
 		return nil, nil, fmt.Errorf("Could not generate certificate - %s", err)
 	}
 
-	// Convert keys into []byte
-	c := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: cert})
-	k := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+	// Convert keys into pem.Block
+	c := &pem.Block{Type: "CERTIFICATE", Bytes: cert}
+	k := &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)}
 	return c, k, nil
 }
--- testcerts_test.go
+++ testcerts_test.go
@@ -1,14 +1,50 @@
 package testcerts
 
 import (
+	"os"
 	"testing"
 )
 
 func TestGeneratingCerts(t *testing.T) {
-	cert, key, err := GenerateCerts()
+	_, _, err := GenerateCerts()
 	if err != nil {
 		t.Errorf("Error while generating certificates - %s", err)
 	}
-	t.Logf("Cert - %s", cert)
-	t.Logf("Key - %s", key)
+}
+
+func TestGeneratingCertsToFile(t *testing.T) {
+	t.Run("Test the happy path", func(t *testing.T) {
+		err := GenerateCertsToFile("/tmp/cert", "/tmp/key")
+		if err != nil {
+			t.Errorf("Error while generating certificates to files - %s", err)
+		}
+
+		// Check if Cert file exists
+		_, err = os.Stat("/tmp/cert")
+		if err != nil {
+			t.Errorf("Error while generating certificates to files file error - %s", err)
+		}
+		_ = os.Remove("/tmp/cert")
+
+		// Check if Key file exists
+		_, err = os.Stat("/tmp/key")
+		if err != nil {
+			t.Errorf("Error while generating certificates to files file error - %s", err)
+		}
+		_ = os.Remove("/tmp/key")
+	})
+
+	t.Run("Testing the unhappy path for cert files", func(t *testing.T) {
+		err := GenerateCertsToFile("/tmp/doesntexist/cert", "/tmp/key")
+		if err == nil {
+			t.Errorf("Expected error when generating a certificate with a bad path got nil")
+		}
+	})
+
+	t.Run("Testing the unhappy path for key files", func(t *testing.T) {
+		err := GenerateCertsToFile("/tmp/cert", "/tmp/doesntexist/key")
+		if err == nil {
+			t.Errorf("Expected error when generating a key with a bad path got nil")
+		}
+	})
 }
