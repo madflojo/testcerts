94cb4298d20345a5f9930a50cd7ff023ff53b6ca
ngergs <niklas@ngergs.de>
2023-10-17 00:21:48 +0200

    chore: coderabbitai adjustments

--- testcerts_test.go
+++ testcerts_test.go
@@ -377,7 +377,7 @@ func testUsingCerts(t *testing.T, rootCAs func(ca *CertificateAuthority, certs *
 	// Setup HTTP Client with Cert Pool
 	certpool := rootCAs(ca, certs)
 	if certpool == nil {
-		t.Error("Test configuration resulted in nil certpool returned from rootCAs func arg")
+		t.Error("Test configuration error: rootCAs arg function returned nil instead of a x509.CertPool")
 		return
 	}
 	client := &http.Client{
